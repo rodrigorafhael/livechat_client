e2ef9191fedf321f2c18e7d5e0d9de3c9ae3b4d4
Rodrigo Rafhael <rodrigorafhael@gmail.com>
2016-03-02 14:50:00 -0400

    move methods from chats model to visitors model

--- lib/livechat/rest/chats.rb
+++ lib/livechat/rest/chats.rb
@@ -6,28 +6,12 @@ module LiveChat
         #chats is different than the other resources
         @list_key = 'chats'
       end
-
-      def new(visitor_id, *args)
-        @client.post "#{@path}/visitors/#{visitor_id}/chat/start", Hash[*args]
-      end
     end
 
     class Chat < InstanceResource
       def send_transcript(*args)
         @client.post "#{@path}/send_transcript", Hash[*args]
         self
-      end
-
-      def get_pending_messages(visitor_id, *args)
-        @client.post "#{@path}/visitors/#{visitor_id}/chat/get_pending_messages", Hash[*args]
-      end
-
-      def send_message(visitor_id, *args)
-        @client.post "#{@path}/visitors/#{visitor_id}/chat/send_message", Hash[*args]
-      end
-
-      def close(visitor_id, *args)
-        @client.post "#{@path}/visitors/#{visitor_id}/chat/close", Hash[*args]
       end
     end
   end
--- lib/livechat/rest/visitors.rb
+++ lib/livechat/rest/visitors.rb
@@ -4,6 +4,22 @@ module LiveChat
       def chatting
         list
       end
+
+      def new(visitor_id, *args)
+        @client.post "#{@path}/visitors/#{visitor_id}/chat/start", Hash[*args]
+      end
+
+      def get_pending_messages(visitor_id, *args)
+        @client.post "#{@path}/visitors/#{visitor_id}/chat/get_pending_messages", Hash[*args]
+      end
+
+      def send_message(visitor_id, *args)
+        @client.post "#{@path}/visitors/#{visitor_id}/chat/send_message", Hash[*args]
+      end
+
+      def close(visitor_id, *args)
+        @client.post "#{@path}/visitors/#{visitor_id}/chat/close", Hash[*args]
+      end
     end
 
     class Visitor < InstanceResource
